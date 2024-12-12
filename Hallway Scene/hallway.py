935af8683d95c4ade1800e9187352c3870e0b432
arsalan eskandrian <arsalan.eskandrian@elev.ga.ntig.se>
2024-12-12 11:02:02 +0100

    More progress into the hallway

--- Hallway Scene/hallway.py
+++ Hallway Scene/hallway.py
@@ -1,13 +1,18 @@
-room = 0 
 def hallway(name):
-    return f"{name} enters the hallway a bit confused but he sees five doors with five different names on them"
+    print(f"{name} enters the hallway a bit confused but he sees five doors with five different names on them")
     room_choice = int(input("What room do you want to enter? "))
     if room_choice == 1:
-        room == 1
+        print("welcome to Finn's room")
     elif room_choice == 2:
-        room == 2
+        print("welcome Rhea's room")
     elif room_choice == 3:
-        room == 3
+        print("welcome Zara's room")
+    elif room_choice == 4:
+        print("welcome Luna's room")
+    elif room_choice == 5:
+        print("welcome Mia's room")
     else:
-        break
-    
+        print("wrong answere")
+
+username = "Arsalan"
+hallo = hallway(username)
