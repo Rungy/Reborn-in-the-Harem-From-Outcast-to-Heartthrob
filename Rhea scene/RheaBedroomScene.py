e366c7b95aaa7ea11e79dcde6e8e43fd3489cb10
Otto karlsson <otto.karlsson@elev.ga.ntig.se>
2024-12-19 11:01:13 +0100

    Rhea scene update

--- Rhea scene/RheaBedroomScene.py
+++ Rhea scene/RheaBedroomScene.py
@@ -1,25 +1,29 @@
 Rheapointtally = 2
-username = 1
-def enterrhearoom (name):
-    return f"{name} enter Rhea's room, you quickly scan the room and catch Rhea doing her morning routine."
-    
-    
-Text1 = enterrhearoom (username)
-print(Text1)
-choice1 = input('''What do you do? 
-        1. Join in 
-        2. Stare 
-        3. Smash''')
-while True:
-    if choice1 == "1":
-        Rheapointtally += 2
-        break
-    elif choice1 == "2":
-        Rheapointtally -= 2
-        break
-    elif choice1 == "3":
-        Rheapointtally -= 3
-        break
-    else:
-        print("Plese enter one of the available numbers!")
+username = "Arsalan"
+def Rheascene1 (name):
+    global Rheapointtally
+    print(f"*{name} enters Rhea's room, you quickly scan the room and catch Rhea doing her morning routine.*")
+   
+    while True:
+        choice1 = input('''What do you do? 
+1. Join in 
+2. Stare 
+3. Smash
+''')
+        if choice1 == "1":
+            print(f"{name} \n*You ask to join Rhea's yoga session, she nods and you join in*")
+            Rheapointtally += 2
+            break
+        elif choice1 == "2":
+            print(f"\n*{name} stands and creeps on Rhea, she notices you staring and mutters to herself* \n-What a wierdo.")
+            Rheapointtally -= 2
+            break
+        elif choice1 == "3":
+            print(f"\n*You run up and hit Rhea across the head causing her to get a headache, she rubs her head in confusion* \n-WTH HELL IS WRONG WITH YOU? \n*She yells at you. {name} decides to apolegize, she forgives you but will not forget your actions*")
+            Rheapointtally -= 3
+            break
+        else:
+            print("\nPlese enter one of the available numbers!")
+        
+Rheascene1(username)
 print(f"Rhea's point tally {Rheapointtally}")
