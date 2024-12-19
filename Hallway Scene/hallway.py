8f49fe00fa1c0196f5cd5294ab976114f0c57ed8
arsalan eskandrian <arsalan.eskandrian@elev.ga.ntig.se>
2024-12-19 10:58:21 +0100

    Improved hallway scene user playability

--- Hallway Scene/hallway.py
+++ Hallway Scene/hallway.py
@@ -3,29 +3,37 @@ def hallway(name):
     global current_room
     print(f"{name} enters the hallway a bit confused but he sees five doors with five different names on them")
     while True:
-        room_choice = int(input("What room do you want to enter? "))
-        if room_choice == 1:
-            print("welcome to Finn's room")
+        room_choice = input("""What room do you want to enter? 
+(following rooms are:)
+1-Finn
+2-Rhea
+3-Zara
+4-Luna
+5-Mia
+(Write the number of the chosen room!)                                                                 
+""")
+        if room_choice == "1":
+            print(f"\n*{name} sneaks in Finn's room*")
             current_room += 1
             break
-        elif room_choice == 2:
-            print("welcome Rhea's room")
+        elif room_choice == "2":
+            print(f"\n*{name} sneaks in Rhea's room*")
             current_room += 2
             break
-        elif room_choice == 3:
-            print("welcome Zara's room")
+        elif room_choice == "3":
+            print(f"\n*{name} sneaks in Zara's room*")
             current_room += 3
             break
-        elif room_choice == 4:
-            print("welcome Luna's room")
+        elif room_choice == "4":
+            print(f"\n*{name} sneaks in Luna's room*")
             current_room += 4
             break
-        elif room_choice == 5:
-            print("welcome Mia's room")
+        elif room_choice == "5":
+            print(f"\n*{name} sneaks in Mia's room*")
             current_room += 5
             break
         else:
-            print("Not a available room")
+            print(f"\nNot an available room {name} you stupid fuck")
 
 username = "Arsalan"
 hallo = hallway(username)
