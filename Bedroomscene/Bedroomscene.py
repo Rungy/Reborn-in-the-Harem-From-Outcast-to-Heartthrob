360954811cdd883b928e20ec94751471b5d4f53e
Otto karlsson <otto.karlsson@elev.ga.ntig.se>
2025-01-16 11:00:21 +0100

    Bedroomscene uncomplete

---
+++ Bedroomscene/Bedroomscene.py
@@ -0,0 +1,12 @@
+username = "Arsalan"
+
+def bedroomscenewakeup (name):
+    print(f"""\n*{name} feels their life slowly fading until it's total darkness.
+\nBut {name} concioussnes remains and he feels an odd sensation as he waits for what feels like an eternity in slience.
+\nThen suddenly {name} hears a voice and the darkness is no more.*
+\nMister wake up mister!
+\n{name} slowly starts to open their eyes, {name} sees 5 foreign figures.")#adda sound effects""")
+
+
+
+bedroomscenewakeup(username)
