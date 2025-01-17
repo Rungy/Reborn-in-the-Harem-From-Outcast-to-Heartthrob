803c004f73a3850fdd6164156f6b81f8b18692ed
Otto karlsson <otto.karlsson@elev.ga.ntig.se>
2025-01-17 16:31:28 +0100

    Bedroomscene unfinshed v2

--- Bedroomscene/Bedroomscene.py
+++ Bedroomscene/Bedroomscene.py
@@ -1,12 +1,37 @@
 username = "Arsalan"
 
 def bedroomscenewakeup (name):
-    print(f"""\n*{name} feels their life slowly fading until it's total darkness.
-\nBut {name} concioussnes remains and he feels an odd sensation as he waits for what feels like an eternity in slience.
-\nThen suddenly {name} hears a voice and the darkness is no more.*
-\nMister wake up mister!
-\n{name} slowly starts to open their eyes, {name} sees 5 foreign figures.")#adda sound effects""")
+    print(f"\n*{name} feels their life slowly fading until it's total darkness")
+    input("\nPress Enter..")
+    print(f"\nBut {name} concioussnes remains and he feels an odd sensation down there as he waits for what feels like an eternity in slience.")
+    input("\nPress Enter..")
+    print(f"\n*Then suddenly {name} hears a voice and the darkness is no more*")
+    input("\nPress Enter..")
+    print("\nMister wake up mister!")
+    input("\nPress Enter..")
+    print(f"\n{name} slowly starts to open their eyes, {name} sees 5 foreign figures with nice curves.")
+    input("\nPress Enter..")
+    print("\nDen första figuren har isblå ögon som genomborrar sin omgivning med skarp intelligens. Hennes tjocka, mörka hår faller elegant runt hennes ansikte och förstärker hennes kraftfulla utstrålning. Hennes drag är skarpa, med höga kindben och en stark käklinje. Hennes kropp är smidig och ståtlig, med en rak och självsäker hållning.")
+    input("\nPress Enter..")
+    print("\nDen andra figuren  har livliga, ljusgröna ögon som gnistrar av nyfikenhet och lekfullhet. Hans mjuka, vågiga hår är ljusbrunt och faller nonchalant runt ansiktet. Hans drag är androgyna och harmoniska, med höga kindben och en uttrycksfull mun. Hans kropp är smidig och graciös, med en avslappnad men självsäker hållning. ")
+    input("\nPress Enter..")
+    print("\nDen Tredje figuren har djupvioletta ögon som tycks rymma hela universum, alltid drömska men skarpa. Hennes långa, mörkblå hår faller i mjuka vågor runt hennes bleka ansikte. Hennes drag är fina och eteriska, med höga kindben och en smal, elegant näsa. Hennes kropp är smal och smidig, med en lugn och nästan svävande närvaro.")
+    input("\nPress Enter..")
+    print("\nhar mjuka, ljusbruna ögon som glittrar av värme och drömmar. Hennes långa, gyllene hår faller i lösa vågor runt hennes ansikte. Hennes drag är mjuka och harmoniska, med rosiga kinder och ett varmt leende. Hennes kropp är smal och graciös, med en avslappnad och nästan svävande närvaro.")
+    input("\nPress Enter..")
+    print("\nhar skarpa, grå ögon som blixtrar av intelligens och attityd. Hennes korta, svartfärgade hår är rufsigt och nonchalant stylat. Hennes drag är distinkta, med höga kindben och en markerad käklinje. Hennes kropp är smidig och stark, med en självsäker och avslappnad hållning.")
+    input("\nPress Enter..")
 
-
+    while True:
+        introduction = input(f"""\nWho do you decide to talk to first?
+1. Figur 1
+2. Figur 2
+3. Figur 3
+4. Figur 4
+5. Figur 5
+""")
+        input("\nPress Enter..")
+        if introduction == "1":
+            print(f"")
 
 bedroomscenewakeup(username)
